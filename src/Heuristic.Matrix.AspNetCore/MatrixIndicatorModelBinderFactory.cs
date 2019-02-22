258a93740a5de904962f16519ba251a775647d29
Robert Huang <robert.h@btigroup.io>
2019-02-22 15:40:56 +0800

    Add model binder and factory

---
+++ src/Heuristic.Matrix.AspNetCore/MatrixIndicatorModelBinderFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+
+namespace Heuristic.Matrix.AspNetCore
+{
+    public class MatrixIndicatorModelBinderFactory : IModelBinderProvider
+    {
+        public static MatrixIndicatorModelBinderFactory Default => new MatrixIndicatorModelBinderFactory();
+
+        IModelBinder IModelBinderProvider.GetBinder(ModelBinderProviderContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            if (context.Metadata.ModelType == typeof(MatrixIndicator))
+            {
+                return MatrixIndicatorModelBinder.Default;
+            }
+            return null;
+        }
+    }
+}
